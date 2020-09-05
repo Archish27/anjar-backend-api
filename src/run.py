413581d11a39f6caa35770435e82d223c119b63d
starzomidev <viral@kustard.io>
2020-09-05 18:45:46 +0530

    create user code refactor

--- src/run.py
+++ src/run.py
@@ -1,6 +1,18 @@
-from flask import Flask, jsonify, make_response
+import uuid
+from flask import Flask, jsonify, make_response, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:root@127.0.0.1:3306/mandir"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    code = db.Column(db.String(10), unique=True, nullable=False)
+    info = db.Column(db.JSON, nullable=False)
 
 
 @app.route('/sample', methods=["GET", "POST"])
@@ -8,6 +20,36 @@ def hello():
     response = make_response(
         jsonify(
             {"message": "hello"}
+        ),
+        200,
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route("/create-customer", methods=["POST"])
+def add_user():
+    code = str(uuid.uuid4())[:8]
+    info = request.form.get("info")
+    user = User(code=code, info=info)
+    db.session.add(user)
+    db.session.commit()
+    response = make_response(
+        jsonify(
+            {"message": "Customer created"}
+        ),
+        200,
+    )
+    # response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route("/get-customers", methods=["GET"])
+def get_users():
+    customers = User.query.all()
+    response = make_response(
+        jsonify(
+            {"customers": customers}
         ),
         200,
     )
