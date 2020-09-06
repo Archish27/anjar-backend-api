b5d5716b254dbbebeff38cdb14a05c76538662ed
starzomidev <viral@kustard.io>
2020-09-06 17:02:09 +0530

    insert user API code refactor

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ class User(db.Model):
     code = db.Column(db.String(10), unique=True, nullable=False)
     name = db.Column(db.TEXT, nullable=False)
     address = db.Column(db.TEXT, nullable=False)
+    phone_number = db.Column(db.String(64), nullable=False)
+    email_id = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(45), nullable=False)
+    plan = db.Column(db.String(45), nullable=False)
+    price = db.Column(db.Numeric(10, 0), nullable=False)
+    members = db.Column(db.Integer, nullable=False)
 
 
 @app.route('/sample', methods=["GET", "POST"])
@@ -30,7 +36,7 @@ def hello():
     return response
 
 
-@app.route("/create", methods=["POST"])
+@app.route("/create-customer", methods=["POST"])
 def add_user():
     code = str(uuid.uuid4())[:8]
     name = request.form.get('name')
@@ -52,39 +58,29 @@ def add_user():
     return response
 
 
-def insert_user(category: str, name: str, code: str, user_name: str, address: str, email: str, phone_number: str):
+def insert_user(category: str, category_name: str, code: str, user_name: str, address: str, email: str, phone_number: str):
     if category:
-        category = name
         category_dict = json.loads(category)
+        print('json---------------', type(category_dict))
         plan = category_dict['name']
         price = category_dict['price']
         members = category_dict['members'] if category_dict['members'] else 0
-        user = User(code=code, user_name=user_name, address=address, email=email,
-                    phone_number=phone_number, category=category, plan=plan, price=price, members=members)
-        print('q------>', db.session.add(user))
+        user = User(code=code, user_name=user_name, address=address, email_id=email, phone_number=phone_number,
+                    category=category_name, plan=plan, price=price, members=members)
+        db.session.add(user)
         db.session.commit()
 
 
 @app.route("/get-customers", methods=["GET"])
 def get_users():
     customers = User.query.all()
-    customer_list = []
-    for customer in customers:
-        customer_list.append({
-            'uuid': customer.code,
-            'info': customer.info
-        })
-    response = make_response(
-        jsonify(
-            {"customers": customer_list}
-        ),
-        200,
-    )
-    return response
+    return render_template('index.html', items=customers)
 
-@app.route("/data", methods=["GET"])
-def get_data():
-    return render_template('index.html')
+
+# @app.route("/data", methods=["GET"])
+# def get_data():
+#     return render_template('index.html', items=get_users())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
